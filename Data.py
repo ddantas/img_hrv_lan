29589a02881654011db8eac6ee511260d4d75ed3
ddantas <dantasu@gmail.com>
2022-01-22 01:12:41 -0300

    Many modifications done to Polar.py and Data.py. Now supporting RR and ECG simultaneously.

--- Data.py
+++ Data.py
@@ -1,37 +1,72 @@
 import numpy as np
 import pandas as pd
+import os
 
+TYPE_RR  = 1
+TYPE_ECG = 2
+
+TIME_UNINITIALIZED = -1
 
 class Data:
+  '''
+  Data objects will store the data received from the device.
+  '''
+
+  def __init__(self, datatype):
+    self.datatype = datatype
+    if (datatype < TYPE_RR and datatype < TYPE_ECG):
+      raise ValueError("Invalid datatype in Data constructor")
+    self.t0 = TIME_UNINITIALIZED
+    self.time = []
+    #TYPE_RR
+    self.values_hr = []
+    self.values_rr = []
+    #TYPE_ECG
+    self.timestamp = []
+    self.values_ecg = []
+
+  def clear(self):
+    self.time = []
+    #TYPE_RR
+    self.values_hr = []
+    self.values_rr = []
+    #TYPE_ECG
+    self.timestamp = []
+    self.values_ecg = []
+
+  ## \brief Remove file if file exists.
+  #
+  #  @param filename File to be removed.
+  @staticmethod
+  def remove(filename):
+    if os.path.exists(filename):
+      os.remove(filename)
+
+  def save_raw_data(self, filename):
     '''
-    Data objects will stores the data received from the device.
+    Save the all triples (time, hr, rr) received from the Polar H10.
+
+    Parameters:
+      filename (string): name from the output file
     '''
+    if os.path.exists(filename):
+      if self.time == []:
+        return
 
-    def __init__(self):
-        self.hr_values = []
-        self.rr_values = []
-        self.t0 = -1
-        self.time = []
+    if (self.datatype == TYPE_RR):
+      df = pd.DataFrame(data=[self.time, self.values_hr, self.values_rr])
+      df = df.T
+      df.columns = ['time', 'heart_rate', 'rr_interval']
+    elif (self.datatype == TYPE_ECG):
+      df = pd.DataFrame(data=[self.time, self.timestamp, self.values_ecg])
+      df = df.T
+      df.columns = ['time', 'timestamp', 'ecg']
+
+    if not os.path.exists(filename):
+      print (f'------ Save raw data in \"{filename}\" ------\n\n')
+      df.to_csv(filename, sep = '\t', mode = "w", header = True)
+    else:
+      df.to_csv(filename, sep = '\t', mode = "a", header = False)
 
 
-    def save_raw_data(self, filename):
-        '''
-        Save the all triples (time, hr, rr) received from the Polar H10.
 
-        Parameters:
-            filename (string): name from the output file
-        '''
-
-        if filename is not None:
-            filename += '_'
-        else:
-            filename = ''
-
-        filename += 'raw_data.csv'
-
-        print (f'------ Save raw data in \"{filename}\" ------\n\n')
-
-        df = pd.DataFrame(data=[self.time, self.hr_values, self.rr_values])
-        df = df.T
-        df.columns = ['time', 'heart rate', 'rr interval']
-        df.to_csv(filename, sep=',', header=True)
--- Polar.py
+++ Polar.py
@@ -28,7 +28,7 @@ import bleak
 from bleak import BleakClient
 from bleak import BleakScanner
 
-from Data import Data
+import Data
 
 
 MODEL_NBR_UUID = '00002a24-0000-1000-8000-00805f9b34fb'
@@ -47,12 +47,8 @@ ECG_WRITE = bytearray([0x02, 0x00, 0x00, 0x01, 0x82, 0x00, 0x01, 0x01, 0x0E, 0x0
 
 interrupt_flag = False
 
-data_rr = Data()
-
-ecg_session_data = []
-ecg_session_time = []
-ecg_session_nowtime = []
-
+data_rr = Data.Data(Data.TYPE_RR)
+data_ecg = Data.Data(Data.TYPE_ECG)
 
 ## \brief Handles the interrupt signal.
 #
@@ -114,20 +110,33 @@ async def print_uuids(d):
 ## REVISAR INICIO
 
 ## Bit conversion of the Hexadecimal stream
-def data_conv(sender, data):
-    if data[0] == 0x00:
-        timestamp = convert_to_unsigned_long(data, 1, 8)
-        step = 3
-        samples = data[10:]
-        offset = 0
-        n=0
-        while offset < len(samples):
-            ecg = convert_array_to_signed_int(samples, offset, step)
-            offset += step
-            now_time = datetime.now()
-            ecg_session_data.extend([ecg])
-            ecg_session_time.extend([timestamp])
-            ecg_session_nowtime.extend([str(now_time)])
+def parse_ecg(sender, data):
+  if data[0] == 0x00:
+    if data_ecg.t0 == Data.TIME_UNINITIALIZED:
+      # Sets t0 to current time
+      data_ecg.t0 = time.time()
+      t = 0.0
+    else:
+      t = time.time() - data_ecg.t0
+    timestamp = convert_to_unsigned_long(data, 1, 8)
+    step = 3
+    samples = data[10:]
+    offset = 0
+    while offset < len(samples):
+      ecg = convert_array_to_signed_int(samples, offset, step)
+      offset += step
+      data_ecg.time.extend([t])
+      data_ecg.timestamp.extend([timestamp])
+      data_ecg.values_ecg.extend([ecg])
+
+    if(data_ecg.time != []):
+      print("data_ecg.time length: %d" % len(data_ecg.time))
+      print("data_ecg.time: " + str(data_ecg.time))
+      print("data_ecg.timestamp length: %d" % len(data_ecg.timestamp))
+      print("data_ecg.timestamp: " + str(data_ecg.timestamp))
+      print("data_ecg.values_ecg length: %d" % len(data_ecg.values_ecg))
+      print("data_ecg.values_ecg: " + str(data_ecg.values_ecg))
+
 
 def convert_array_to_signed_int(data, offset, length):
     return int.from_bytes(
@@ -159,12 +168,12 @@ def parse_rr(sender, data):
             Byte 2..5 - UINT16 RR intervals
     '''
     # If the 4th bit of the bytearray is 1 then RR reads were sent
-    if data[0] & 0b00010000 == 0b00010000:
+    if (data[0] & 0b00010000):
 
-        if data_rr.time == []:
+        if data_rr.t0 == Data.TIME_UNINITIALIZED:
             # Sets t0 to current time
             data_rr.t0 = time.time()
-            t = 0
+            t = 0.0
         else:
             t = time.time() - data_rr.t0
 
@@ -172,12 +181,12 @@ def parse_rr(sender, data):
 
         # data[1] is the HR read
         hr = data[1]
-        data_rr.hr_values.append(hr)
+        data_rr.values_hr.append(hr)
 
         # data[2:4] is the RR interval
         # Convert the bytes in UINT16
         rr = int.from_bytes(data[2:4], byteorder='little', signed=False)
-        data_rr.rr_values.append(rr)
+        data_rr.values_rr.append(rr)
 
         print(f'Time: {t} s,   Heart rate: {hr} bpm,       RR-interval: {rr} ms')
 
@@ -188,7 +197,8 @@ def parse_rr(sender, data):
 ## \brief Receive RR data from Polar sensor
 #
 #  @param d Device.
-async def receive_rr(d):
+#  @param filename File where RR data will be stored.
+async def receive_rr(d, filename):
   # Listen to the SIGINT signal
   signal.signal(signal.SIGINT, signal_handler)
   try:
@@ -199,20 +209,21 @@ async def receive_rr(d):
       await client.start_notify(HEART_RATE, parse_rr)
       while not interrupt_flag:
         await asyncio.sleep(0.1)
+        data_rr.save_raw_data(filename)
+        if(data_rr.time != []):
+          data_rr.clear()
       # Will disconnect
       await client.stop_notify(HEART_RATE)
       await client.disconnect()
     
   except Exception as e:
-    print_exception(e, "connect", __file__)
+    print_exception(e, "receive_rr", __file__)
 
 ## \brief Receive ECG data from Polar sensor
 #
 #  @param d Device.
-async def receive_ecg(d):
-  global ecg_session_data
-  global ecg_session_time
-  global ecg_session_nowtime
+#  @param filename File where ECG data will be stored.
+async def receive_ecg(d, filename):
   # Listen to the SIGINT signal
   signal.signal(signal.SIGINT, signal_handler)
   try:
@@ -222,83 +233,54 @@ async def receive_ecg(d):
       # Connected
       att_read = await client.read_gatt_char(PMD_CONTROL)
       await client.write_gatt_char(PMD_CONTROL, ECG_WRITE)
-      await client.start_notify(PMD_DATA, data_conv)
+      await client.start_notify(PMD_DATA, parse_ecg)
       while not interrupt_flag:
         await asyncio.sleep(0.1)
-        if(ecg_session_data != []):
-          print("ecg_session_nowtime length: %d" % len(ecg_session_nowtime))
-          print("ecg_session_nowtime: " + str(ecg_session_nowtime))
-          print("ecg_session_time length: %d" % len(ecg_session_time))
-          print("ecg_session_time: " + str(ecg_session_time))
-          print("ecg_session_data length: %d" % len(ecg_session_data))
-          print("ecg_session_data: " + str(ecg_session_data))
-          ecg_session_nowtime = []
-          ecg_session_time = []
-          ecg_session_data = []
+        data_ecg.save_raw_data(filename)
+        if(data_ecg.time != []):
+          data_ecg.clear()
+
+      # Will disconnect
+      await client.stop_notify(PMD_DATA)
+      await client.stop_notify(HEART_RATE)
+      await client.disconnect()
+
+  except Exception as e:
+    print_exception(e, "receive_both", __file__)
+
+## \brief Receive ECG and RR data from Polar sensor
+#
+#  @param d Device.
+#  @param filename_rr File where RR data will be stored.
+#  @param filename_ecg File where ECG data will be stored.
+async def receive_both(d, filename_rr, filename_ecg):
+  # Listen to the SIGINT signal
+  signal.signal(signal.SIGINT, signal_handler)
+  try:
+    async with BleakClient(d.address) as client:
+      if (not client.is_connected):
+        raise Exception("Unable to connect to device at %s" % d.address)
+      # Connected
+      att_read = await client.read_gatt_char(PMD_CONTROL)
+      await client.write_gatt_char(PMD_CONTROL, ECG_WRITE)
+      await client.start_notify(PMD_DATA, parse_ecg)
+      await client.start_notify(HEART_RATE, parse_rr)
+      while not interrupt_flag:
+        await asyncio.sleep(0.1)
+        data_ecg.save_raw_data(filename_ecg)
+        data_rr.save_raw_data(filename_rr)
+        if(data_ecg.time != []):
+          data_ecg.clear()
+        if(data_rr.time != []):
+          data_rr.clear()
 
       # Will disconnect
       await client.stop_notify(PMD_DATA)
       await client.disconnect()
+
     
   except Exception as e:
     print_exception(e, "connect", __file__)
-
-"""
-    global collect_ecg
-    ## Writing chracterstic description to control point for request of UUID (defined above) ##
-
-    await client.is_connected()
-    print("---------Device connected--------------")
-
-    model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-    print("Model Number: {0}".format("".join(map(chr, model_number))))
-
-    manufacturer_name = await client.read_gatt_char(MANUFACTURER_NAME_UUID)
-    print("Manufacturer Name: {0}".format("".join(map(chr, manufacturer_name))))
-
-    battery_level = await client.read_gatt_char(BATTERY_LEVEL_UUID)
-    print("Battery Level: {0}%".format(int(battery_level[0])))
-    battery_limit = 30
-
-    att_read = await client.read_gatt_char(PMD_CONTROL)
-
-    await client.write_gatt_char(PMD_CONTROL, ECG_WRITE)
-
-    ## ECG stream started
-    await client.start_notify(PMD_DATA, data_conv)
-
-    print("Collecting ECG data...")
-    
-    while collect_ecg:
-        ## Collecting ECG data for 1 second
-        await asyncio.sleep(1)
-                
-        #global ecg_session_data
-        filePath = "./data/"+file_name_ecg+".csv"
-
-        if(ecg_session_data==[]):
-            print("[warning]:data is None so cant save to",filePath)
-        else:
-            print("ecg_session_time length: %d" % len(ecg_session_time))
-            print("ecg_session_time: " + str(ecg_session_time))
-            print("ecg_session_data length: %d" % len(ecg_session_data))
-            print("ecg_session_data: " + str(ecg_session_data))
-            
-            savetocsv(filePath)
-            if(battery_level[0] < battery_limit):
-                print("[Warning]:Battery Level: {0}%".format(int(battery_level[0])))
-                battery_limit -= 5
-
-
-    ## Stop the stream once data is collected
-    await client.stop_notify(PMD_DATA)
-    
-    print("Stopping ECG data...")
-    print("[CLOSED] application closed.")
-    
-    sys.exit(0)
-"""
-
 
 ## \brief Print details about device.
 #
@@ -365,8 +347,16 @@ def main():
     return
 
   d = devices[0]
-  #asyncio.run(receive_rr(d))
-  asyncio.run(receive_ecg(d))
+
+  filename_rr = "/tmp/rr.tsv"
+  Data.Data.remove(filename_rr)
+  #asyncio.run(receive_rr(d, filename_rr))
+
+  filename_ecg = "/tmp/ecg.tsv"
+  Data.Data.remove(filename_ecg)
+  #asyncio.run(receive_ecg(d, filename_ecg))
+
+  asyncio.run(receive_both(d, filename_rr, filename_ecg))
   
   #task = asyncio.create_task(receive_rr(d))
   #await asyncio.gather(task)
