d8324bb0404bd95ec68c237dbadb0fc3cb7d8f3c
ddantas <dantasu@gmail.com>
2022-03-22 21:03:48 -0300

    Added print_message and print_exception to utils.py.

--- utils.py
+++ utils.py
@@ -3,26 +3,46 @@ import numpy as np
 from biosppy.signals import ecg
 
 
-# Remove?
-def adjust_filename_old(filename, file_suffix):
-
-	dir_tree = filename.split('/')
-	filename = dir_tree[-1]
-	cur_dir = '/'.join(dir_tree[:-1])
-
-	if not os.path.exists(cur_dir + '/02_preprocess') and '02_preprocess' not in cur_dir:
-		os.mkdir(cur_dir + '/02_preprocess')
-
-	new_file = filename[:-4] + file_suffix
-	if '02_preprocess' in cur_dir:
-		save_dir = cur_dir + '/'
-
-	else:
-		save_dir = cur_dir + '/02_preprocess/'
-
-	return save_dir + new_file
-
+## \brief Replace extension with suffix.
+#
+#  @param filename Filename to be adjusted.
+#  @param file_suffix Replace file extension.
 def adjust_filename(filename, file_suffix):
-        basename, file_extension = os.path.splitext(filename)
-        result = basename + file_suffix
-        return result
+  basename, file_extension = os.path.splitext(filename)
+  result = basename + file_suffix
+  return result
+
+## \brief Print exception message preceded with file and function name.
+#
+#  @param msg Exception message.
+#  @param func Function name.
+#  @param file File name. Caller may use __file__.
+def print_exception(msg, func, file):
+  print("Exception at %s: %s: %s" % (os.path.basename(file), func, msg))
+
+## \brief Print message preceded with file and function name.
+#
+#  @param msg Message.
+#  @param func Function name.
+#  @param file File name. Caller may use __file__.
+def print_message(msg, func, file):
+  print("%s: %s: %s" % (os.path.basename(file), func, msg))
+
+## \brief Print message preceded with file and function name.
+#
+#  @param msg Message.
+#  @param func Function name.
+#  @param file File name. Caller may use __file__.
+def test_message():
+  print_message("Hello, world!", test_message.__name__, __file__)
+
+"""#########################################################
+############################################################
+### Main function                                        ###
+############################################################
+#########################################################"""
+
+
+if __name__ == "__main__":
+
+  test_message()
