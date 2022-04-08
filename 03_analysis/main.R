1be0826b02dd808496959738634e710e115754d1
Joao Lobo <j.lobo.pev@gmail.com>
2022-04-08 17:22:57 -0300

    Create a function to concatenate all datasets into one and change the way statistical data is extracted from the final dataset

--- 03_analysis/main.R
+++ 03_analysis/main.R
@@ -1,16 +1,19 @@
-
-
 source("utils.R")
 source("analysis.R")
 
-filename = "../data/a003/02_preprocess/dataset.tsv"
+filename = "output.tsv"
 
-df = load_data(filename)
-
+folders = commandArgs(trailingOnly=TRUE)
+print("Getting datasets from folders")
+print(folders)
+concatenate_datasets(folders)
 
 imit = TRUE
 sync = NA
 
 df = load_data(filename)
-#result = get_SI_stats(filename, imit, sync)
-stats_df = create_SI_dataframe(df)
+folder_names = get_folder_names(df)
+stats_df = create_SI_dataframe(df, folder_names)
+df = get_stats(stats_df)
+
+print(df)
