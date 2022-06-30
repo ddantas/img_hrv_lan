2c4463f518270410a873e51e37196503a925961b
ddantas <dantasu@gmail.com>
2022-06-30 12:36:40 -0300

    Modified optical flow.

--- 03_optical_flow/optical_flow.py
+++ 03_optical_flow/optical_flow.py
@@ -16,6 +16,7 @@ from queue import Queue
 import sys
 import pandas as pd
 import numpy as np
+import cv2
 
 #import rr_interpolation
 #import rr_inference
@@ -55,8 +56,6 @@ def create_data_file(input_path,
 
 def write_to_dataset0(filename_input, filename_output):
 
-  import numpy as np
-  import cv2
   cap = cv2.VideoCapture(filename_input)
   f = 0
   while(cap.isOpened()):
@@ -99,6 +98,57 @@ def find_centroid(img):
 
   return cx, cy
 
+def apply_mask(img, mask):
+  # Grayscale
+  if (len(img.shape) <= 2):
+    result = np.minimum(img, mask)
+  else:
+    result = np.zeros(img.shape)
+    n_ch = img.shape[2]
+    for c in range(n_ch):
+      result[:, :, c] = np.minimum(img[:, :, c], mask)
+  return result
+
+def find_flow(flow, th=3, magnitude=None):
+  x_flow = flow[:, :, 0]
+  y_flow = flow[:, :, 1]
+
+  if (magnitude is None):
+    magnitude, angle = cv2.cartToPolar(x_flow, y_flow)
+  ret, mask = cv2.threshold(magnitude, th, 255, cv2.THRESH_BINARY)
+  n_pix = sum(sum(mask)) // 255
+  print("n_pix " + str(n_pix))
+
+  import my
+  my.imshow(mask)
+
+  flow_masked = apply_mask(flow, mask)
+  x_flow_masked = flow_masked[:, :, 0]
+  y_flow_masked = flow_masked[:, :, 1]
+
+  #return flow_masked
+
+  x_tot = sum(sum(abs(x_flow_masked)))
+  y_tot = sum(sum(abs(y_flow_masked)))
+  #h, w = aux.shape[0:2]
+
+  x_sum_abs = sum(abs(x_flow_masked))
+  x_sum = sum(x_flow_masked)
+  x_coord = range(len(x_sum))
+  print("x_sum_abs " + str(x_sum_abs))
+  print("x_coord " + str(x_coord))
+  print("x_tot " + str(x_tot))
+  cx = np.dot(x_sum_abs, x_coord) / x_tot
+  vx = sum(x_sum) / n_pix
+
+  y_sum_abs = sum(abs(y_flow_masked.transpose()))
+  y_sum = sum(y_flow_masked.transpose())
+  y_coord = range(len(y_sum))
+  cy = np.dot(y_sum_abs, y_coord) / y_tot
+  vy = sum(y_sum) / n_pix
+
+  return cx, cy, vx, vy
+
 def clamp(c, inf, sup):
   c = max(inf, c)
   c = min(sup, c)
@@ -123,7 +173,7 @@ def draw_cross(img, x, y, r=2):
   for x_i in range(x_min, x_max + 1):
     img[round(y), round(x_i)] = 255
 
-def write_to_dataset1(filename_input, filename_output):
+def write_to_dataset_diff(filename_input, filename_output):
 
   import numpy as np
   import cv2
@@ -178,7 +228,7 @@ def write_to_dataset1(filename_input, filename_output):
   cv2.destroyAllWindows()
   return
 
-def write_to_dataset2(filename_input, filename_output):
+def write_to_dataset_dense(filename_input, filename_output):
 
   import numpy as np
   import cv2
@@ -191,16 +241,19 @@ def write_to_dataset2(filename_input, filename_output):
     ret, frame = cap.read()
     frame[0:24, :] = 0
     frame_left, frame_right = split(frame)
+    if (f == 0):
+      print(f)
+      gray0 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      w_half = frame.shape[1] // 2
+      mask = np.zeros_like(frame)
+      mask[..., 1] = 255
+      f += 1
+    #elif (f < 3000):
+    #  f += 1
+    #  continue
     if (f % 2 == 0):
       gray0 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
       #gray0[0:24, :] = 0
-      if (f == 0):
-        print(f)
-        w_half = frame.shape[1] // 2
-        mask = np.zeros_like(frame)
-        mask[..., 1] = 255
-        f += 1
-        continue
       frame_new = gray0
       frame_old = gray1
     else:
@@ -208,6 +261,7 @@ def write_to_dataset2(filename_input, filename_output):
       frame_new = gray1
       frame_old = gray0
       #gray1[0:24, :] = 0
+      
 
     flow = cv2.calcOpticalFlowFarneback(frame_old, frame_new, None, pyr_scale = 0.5, levels = 5, winsize = 11, iterations = 5, poly_n = 5, poly_sigma = 1.1, flags = 0)
     # Compute the magnitude and angle of the 2D vectors
@@ -216,24 +270,35 @@ def write_to_dataset2(filename_input, filename_output):
     mask[..., 0] = angle * 180 / np.pi / 2
     # Set image value according to the optical flow magnitude (normalized)
     #mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
-    mask[..., 2] = magnitude
+    mask[..., 2] = 10 * magnitude
     # Convert HSV to RGB (BGR) color representation
     rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
 
-    l, r = split(magnitude)
-    lxm, lym, lvm = find_maximum(l)
-    rxm, rym, rvm = find_maximum(r)
-    if (lvm > 255):
-      lvm = 255
-    if (rvm > 255):
-      rvm = 255
-    lvm = round(lvm)
-    rvm = round(rvm)
-    print("%d: L[%d, %d] = %d  R[%d, %d] = %d" % (f, lxm, lym, lvm, rxm, rym, rvm))
+    th = 0
+    #flow_masked = find_flow(flow, th, magnitude)
+    #ret, mask2 = cv2.threshold(magnitude, th, 255, cv2.THRESH_BINARY)
+
+    #magnitude[magnitude > 255] = 0
+    l_flow, r_flow = split(flow)
+    l_mag, r_mag = split(magnitude)
+    print("l_flow.shape " + str(l_flow.shape))
+    print("r_flow.shape " + str(r_flow.shape))
+    print("l_mag.shape " + str(l_mag.shape))
+    print("r_mag.shape " + str(r_mag.shape))
+
+    l_cx, l_cy, l_vx, l_vy = find_flow(l_flow, th, l_mag)
+    r_cx, r_cy, r_vx, r_vy = find_flow(r_flow, th, r_mag)
+    #if (lvm > 255):
+    #  lvm = 255
+    #if (rvm > 255):
+    #  rvm = 255
+    #lvm = round(lvm)
+    #rvm = round(rvm)
+    print("%d: L[%d, %d] + (%f, %f)  R[%d, %d] + (%f, %f)" % (f, l_cx, l_cy, l_vx, l_vy, r_cx, r_cy, r_vx, r_vy))
 
     #mask = mask.astype(np.uint8)
-    draw_cross(rgb, lxm, lym, lvm // 10)
-    draw_cross(rgb, w_half + rxm, rym, rvm // 10)
+    draw_cross(rgb,          round(l_cx), round(l_cy), round(l_vx + l_vy) // 1)
+    draw_cross(rgb, w_half + round(r_cx), round(r_cy), round(r_vx + r_vy) // 1)
     
     # save start
     #filename = "f%05d.tiff" % f
@@ -241,12 +306,24 @@ def write_to_dataset2(filename_input, filename_output):
     # save end
 
     dense_flow = cv2.addWeighted(frame, 1, rgb, 2, 0)
+
+    print("np.amax(flow) = " + str(np.amax(flow)))
+    print("np.amin(flow) = " + str(np.amin(flow)))
+    #print(flow[:, 0, :])
+    print("flow.shape = " + str(flow.shape))
+    print("magnitude.shape = " + str(magnitude.shape))
+
+    flow2 = np.zeros(frame.shape, dtype=np.uint8)
+    print(flow2.dtype)
+    flow2[:, :, 2] = 10*flow[:,:,0] + 128
+    flow2[:, :, 1] = 10*flow[:,:,1] + 128
+    #flow2[:, :, 0] = 128
     
-    cv2.imshow('frame', rgb)
+    cv2.imshow('frame', flow2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     f += 1
-    if (f > 3000):
+    if (f > 0):
       input()
 
   cap.release()
@@ -292,7 +369,7 @@ def main(path_input, path_output):
       print("filename_dataset: " + filename_dataset)      
       print("filename_input: " + filename_input)
       print("filename_output: " + filename_output)
-      write_to_dataset2(filename_input, filename_output)
+      write_to_dataset_dense(filename_input, filename_output)
       input("Press Enter...")
 
       
