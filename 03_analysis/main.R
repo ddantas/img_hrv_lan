27844b5ea9edb3b2b0eb5898f4fddd237dcef744
ddantas <dantasu@gmail.com>
2022-10-14 18:17:12 -0300

    Analysis step generating concatenated dataset.

--- 03_analysis/main.R
+++ 03_analysis/main.R
@@ -8,6 +8,8 @@ library(stringr)
 
 library(ggplot2)
 
+GENERATE = TRUE
+REPORT   = FALSE
 
 source("utils.R")
 source("analysis.R")
@@ -21,67 +23,24 @@ print(folders)
 
 if (length(folders) == 0)
 {
-  folders = c("../data/b001",
-              "../data/b002",
-              "../data/b003",
-              "../data/b004",
-              "../data/b009",
-              "../data/b010",
-              "../data/b011",
-              "../data/b012",
-              "../data/b013",
-              "../data/b014",
-              "../data/b015",
-              "../data/b016",
-              "../data/b017",
-              "../data/b018",
-              "../data/b019",
-              "../data/b020",
-              "../data/b021",
-              "../data/b022",
-              "../data/b023",
-              "../data/b024",
-              "../data/b025",
-              "../data/b026",
-              "../data/b027",
-              "../data/b028",
-              "../data/b029",
-              "../data/b030",
-              "../data/b031",
-              "../data/b032",
-              "../data/b033",
-              "../data/b034",
-              "../data/b035",
-              "../data/b036",
-              "../data/b037",
-              "../data/b038",
-              "../data/b039",
-              "../data/b040",
-              "../data/b041",
-              "../data/b042",
-              "../data/b043",
-              "../data/b044")
-  #folders = c("../data/b044")
+  folders = c("../data/001",
+              "../data/002",
+              "../data/003",
+              "../data/004",
+              "../data/005")
+  folders = c("../data/001")
 }
 
-ds_files = c("02_preprocess/dataset_dd.tsv", "02_preprocess/dataset_jf.tsv")
-#ds_files = c("02_preprocess/dataset_dd.tsv")
-#ds_files = c("02_preprocess/dataset_jf.tsv")
+#ds_files = c("02_preprocess/dataset_dd.tsv", "02_preprocess/dataset_jf.tsv")
+ds_files = c("02_preprocess/dataset.tsv")
 
 filename_dataset = "dataset.tsv"
-#concatenate_datasets(folders, filename_dataset, ds_files)
+if (GENERATE)
+{
+  concatenate_datasets(folders, filename_dataset, ds_files)
+}
 
-filename_dataset_stack = "dataset_stack.tsv"
-dataset_stack(filename_dataset, filename_dataset_stack)
-
-###############################################################3
-#folder_names = get_folder_names(df)
-#stats_df = create_SI_dataframe(df, folder_names)
-#result = get_stats(stats_df)
-#print(result)
-
-df = load_data(filename_dataset)
-df_stack = load_data(filename_dataset_stack)
+###########################################################
 
 confidence = 0.95
 prompt     = 0
@@ -89,9 +48,11 @@ inputFile     = filename_dataset
 outputDir      = "saida"
 outputFile     = "report.html"
 
-report(inputFile, outputFile, outputDir, df, df_stack, confidence, prompt)
+if (REPORT)
+{
+  report(inputFile, outputFile, outputDir, df, df_stack, confidence, prompt)
+}
 
-
-###############################################################3
+###########################################################
 
 #group_roles(filename)
--- 03_analysis/utils.R
+++ 03_analysis/utils.R
@@ -7,17 +7,9 @@ load_data <- function(filename){
 
 concatenate_datasets <- function(folders, filename_output, ds_files) {
 
-  columns = c("folder", "annotator", "block", "label", "time",
-              "IsImit", "IsSync", "Imitator", "Model",
+  columns = c("folder", "time", "block", "slide",
               "hr_subj1_linear", "hr_subj2_linear", "hr_subj1_nearest", "hr_subj2_nearest",
-              "hr_subj1_ecg_linear", "hr_subj2_ecg_linear", "hr_subj1_ecg_nearest",
-              "hr_subj2_ecg_nearest", "msg1", "msg2", "Gender")
-  set_male = c("b005", "b006", "b007", "b008",
-               "b009", "b010", "b011", "b012",
-               "b013", "b014", "b015", "b016",
-               "b021", "b022", "b023", "b024",
-               "b025", "b026", "b027", "b028",
-               "b033", "b034", "b035", "b036")
+              "hr_subj1_ecg_linear", "hr_subj2_ecg_linear", "hr_subj1_ecg_nearest")
 
   print(columns)
 
@@ -32,63 +24,6 @@ concatenate_datasets <- function(folders, filename_output, ds_files) {
       ds_path = file.path(f, default_ds_subpath)
       df = load_data(ds_path)
 
-      # Changing NA to ""
-      #df2[is.na(df2$IsImit), 'IsImit'] = ""
-      #df2[is.na(df2$IsSync), 'IsSync'] = ""
-      #df2[is.na(df2$Imitator), 'Imitator'] = ""
-      #df2[is.na(df2$Model), 'Model'] = ""
-
-      # If IsImit is TRUE, Imitator must not be NA
-      n = length(df[(!is.na(df$IsImit) & df$IsImit == TRUE & is.na(df$Imitator)), "IsSync"])
-      if (n > 0)
-      {
-        writeLines(paste("Found", n, "lines with IsImit = TRUE and Imitator == NA. Fatal error. Please review annotation..."))
-        writeLines("")
-        stop
-      }
-      # If IsImit is TRUE, IsSync must not be NA
-      n = length(df[(!is.na(df$IsImit) & df$IsImit == TRUE & is.na(df$IsSync)), "IsSync"])
-      if (n > 0)
-      {
-        writeLines(paste("Found", n, "lines with IsImit = TRUE and IsSync == NA. Correcting..."))
-        writeLines("")
-        df[(!is.na(df$IsImit) & df$IsImit == FALSE & is.na(df$IsSync)), "IsSync"] = FALSE
-      }
-      # If IsImit is FALSE, IsSync must be NA
-      n = length(df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$IsSync)), "IsSync"])
-      if (n > 0)
-      {
-        writeLines(paste("Found", n, "lines with IsImit = FALSE and IsSync != NA. Correcting..."))
-        writeLines("")
-        df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$IsSync)), "IsSync"] = NA
-      }
-      # If IsImit is FALSE, Imitator must be NA
-      n = length(df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$Imitator)), "IsSync"])
-      if (n > 0)
-      {
-        writeLines(paste("Found", n, "lines with IsImit = FALSE and Imitator != NA. Correcting..."))
-        writeLines("")
-        df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$Imitator)), "Imitator"] = NA
-      }
-      # If IsImit is FALSE, Model must be NA
-      n = length(df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$Model)), "IsSync"])
-      if (n > 0)
-      {
-        writeLines(paste("Found", n, "lines with IsImit = FALSE and Model != NA. Correcting..."))
-        writeLines("")
-        df[(!is.na(df$IsImit) & df$IsImit == FALSE & !is.na(df$Model)), "Model"] = NA
-      }
-      # Fill Model column
-      df$Model = 3 - df$Imitator
-      # Fill Gender column
-      if (df$folder %in% set_male)
-      {
-        df$Gender = "M"
-      }
-      else
-      {
-        df$Gender = "F"
-      }  
       # Fill derivative  columns
       df$d_hr_subj1_linear      = derivate(df$hr_subj1_linear)
       df$d_hr_subj2_linear      = derivate(df$hr_subj2_linear)
@@ -103,20 +38,6 @@ concatenate_datasets <- function(folders, filename_output, ds_files) {
       final_df = rbind(final_df, df)
     }
   }
-  final_df$block <- as.character(final_df$block)
-  final_df$block[final_df$label == "Prelude"] = "block0"
-  final_df$block[final_df$block == "block2"] = "block3"
-  final_df$block[final_df$label == "Interlude"] = "block2"
-  final_df$block <- as.factor(final_df$block)
-
-  #final_df[is.na(final_df$IsImit), 'IsImit'] = ""
-  #final_df[is.na(final_df$IsSync), 'IsSync'] = ""
-  #final_df[is.na(final_df$Imitator), 'Imitator'] = ""
-  #final_df[is.na(final_df$Model), 'Model'] = ""
-  #df2[is.na(df2$IsImit), 'IsImit'] = ""
-  #df2[is.na(df2$IsSync), 'IsSync'] = ""
-  #df2[is.na(df2$Imitator), 'Imitator'] = ""
-  #df2[is.na(df2$Model), 'Model'] = ""
 
   print(colnames(final_df))
   
